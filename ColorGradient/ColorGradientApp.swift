036b4a73994be97bb38c096252e56d82bac6399f
hajdinim <hajdinim@uni.coventry.ac.uk>
2020-11-07 17:58:27 +0000

    Initial Commit

---
+++ ColorGradient/ColorGradientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorGradientApp.swift
+//  ColorGradient
+//
+//  Created by Tony Hajdini on 07/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ColorGradientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
